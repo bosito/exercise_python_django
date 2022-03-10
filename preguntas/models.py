d86b6eb045c331c06a639fa2f6ce4ac4089b453d
LuisLara28 <luis_28_92lara@hotmail.com>
2022-03-09 21:34:05 -0600

    actualizacion-3

--- preguntas/models.py
+++ preguntas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Pregunta(models.Model):
-    pregunta = models.CharField(max_length=255)
+    pregunta = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
 
@@ -10,7 +10,7 @@ class Pregunta(models.Model):
         return self.pregunta
 
 class Respuesta(models.Model):
-    respuesta = models.CharField(max_length=255)
+    respuesta = models.TextField()
     owner = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
     date = models.DateField()
 
